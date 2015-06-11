7427fa810b207802efd7cb9be12951da7fdfd6b3
Segun <xtreem88@gmail.com>
2015-06-11 15:54:27 -0700

    Optimized to run faster

--- wecycler.py
+++ wecycler.py
@@ -1,7 +1,18 @@
+from time import clock
+def check_repeated_number(number):
+  repeated=False
+  number=str(number)
+  for i in range(0,len(number)-2):
+    if number[i] == number[i+1]:
+      repeated = True
+    else:
+      repeated = False
+  return repeated
+start = clock()
 f=open('.input')
 numbers=int(f.readline().rstrip('\r\n'))
 i=1
-out=open('.output','w')
+out=open('.output2','w')
 while i<=numbers:
   read=f.readline().rstrip('\r\n')
   line=read.split()
@@ -10,13 +21,18 @@ while i<=numbers:
   total=0
   c=[x for x in range(a,b+1)]
   for key,x in enumerate(c):
+    if check_repeated_number(x) == False:
       for y in c[key+1:]:
           n=str(x)
           m=str(y)
           if len(n)==len(m) and len(n)>1 and len(m)>1:
              ki=1 
              while ki <len(n):
+                 
                  temp=n[-1:-(ki)-1:-1][::-1]+n[:-(ki)]
+                 if temp == n:
+                   break
+                 
                  if temp[0]!='0':
                   if temp==m:
                      total+=1
@@ -31,3 +47,4 @@ while i<=numbers:
   i+=1
 out.close()
 f.close()
+print ("Total time: "+`clock() - start`)
